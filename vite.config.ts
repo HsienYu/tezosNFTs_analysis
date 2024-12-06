0a2ed7788457c1ac6d78be81f5cb19872fb4b6af
chenghsienyu <chenghsienyu@aim.com>
2024-12-06 18:19:59 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 export default defineConfig({
   plugins: [svelte()],
   build: {
-    outDir: 'public/build',
+    outDir: 'build', // Change this to 'build'
     emptyOutDir: true
   },
-  publicDir: 'static' // Change this to a different folder
+  publicDir: 'static'
 });
